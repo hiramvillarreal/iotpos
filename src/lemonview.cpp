8586b9527110b2bc55dc7bbe73085787ec05cb11
hiramvillarreal <hiramvillarreal@hotmail.com>
2015-12-12 11:23:21 -0700

    Rute correction to balance report spool

--- src/lemonview.cpp
+++ src/lemonview.cpp
@@ -1,7 +1,8 @@
 /**************************************************************************
  *   Copyright © 2007-2011 by Miguel Chavez Gamboa                         *
  *   miguel@lemonpos.org                                                   *
- *                                                                         *
+ *   Modified by Daniel A. Cervantes Cabrera *
+ *   dcchivela@gmail.com                                                                        *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
@@ -4340,7 +4341,7 @@ void lemonView::printBalance(QStringList lines)
       QString printerCodec=Settings::printerCodec();
       qDebug()<<"[Printing balance on "<<printerFile<<"]";
       PrintDEV::printSmallBalance(printerFile, printerCodec, lines.join("\n"));
-      QFile fOut("./raspberrypos/printing/spool");
+      QFile fOut("./iotpos/printing/spool");
                if (fOut.open(QFile::WriteOnly | QFile::Text)) {
                    QTextStream s(&fOut);
                    for (int i = 0; i < lines.size(); ++i)
