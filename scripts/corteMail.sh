a87ba7ede1aa9b26e81654c2c653a03fa1ca4eb5
hiramvillarreal <hiramvillarreal@hotmail.com>
2015-12-25 16:57:37 -0700

    Correction scrips mail

--- scripts/corteMail.sh
+++ scripts/corteMail.sh
@@ -1,1 +1,1 @@
- tail -n 20 /home/pi/iotpos/printing/spool | mail -s "Corte de Caja" hiramvillarreal.ap@gmail.com
+tail -n 1000 /home/pi/iotpos/printing/spool | mail -s "Corte de Caja" hiramvillarreal.ap@gmail.com
