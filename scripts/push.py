5f019df6ed45bea5baa1eaacdf05e237b1a8050d
Hiram Villarreal <hiramvillarreal@hotmail.com>
2016-01-13 12:18:28 -0700

    Update push.py
    Correction , print without pressing the bottom from las call

--- scripts/push.py
+++ scripts/push.py
@@ -1,29 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 import os
+ledPin       = 18
+buttonPin    = 23
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 on_time = 0.1 # time led is ON in seconds
 off_time = 0.1 # time led is OFF in seconds
-counter = 0 
+counter = 0
 
 
 while counter <=60:
-    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(ledPin, GPIO.HIGH)
     time.sleep(on_time)
-    GPIO.output(18, GPIO.LOW)
+    GPIO.output(ledPin, GPIO.LOW)
     time.sleep(off_time)
-    input_state = GPIO.input(23)
+    input_state = GPIO.input(buttonPin)
     counter += 1
     if input_state == False:
         print('Button Pressed')
         os.system ("cat /home/pi/iotpos/printing/spool > /dev/ttyAMA0")
-        GPIO.cleanup()
         time.sleep(0.2)
-        exit()
+        break;
 GPIO.cleanup()
 exit()
-
