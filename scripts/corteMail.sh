f1fecafc2132a35ccde7dc4fe9fd0a56ba303c42
Hiram Villarreal <hiramvillarreal@hotmail.com>
2016-01-26 14:18:59 -0700

    Update corteMail.sh

--- scripts/corteMail.sh
+++ scripts/corteMail.sh
@@ -1,1 +1,1 @@
-tail -n 1000 /home/pi/iotpos/printing/spool | mail -s "Corte de Caja" hiramvillarreal.ap@gmail.com
+tail -n 1000 /home/pi/iotpos/printing/spool | mail -s "Corte de Caja" your_mail@gmail.com
