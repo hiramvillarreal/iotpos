d62171a19d7de90a700e86de9a8852457b5ad1dc
Hiram Villarreal <hiramvillarreal@hotmail.com>
2016-02-03 12:10:29 -0700

    Delete fix_0.9.sql

--- database_resources/fix_0.9.sql
+++
@@ -1,8 +0,0 @@
-# Fixes to migrate from 0.9 to 0.9.1 database version
-# (C) Miguel Chavez Gamboa 2009-2010 [GPL v2 or later]
-
-use lemondb;
-
-alter table transactionitems add `tax` double default 0 after deliveryDateTime;
-alter table transactions add `totalTax` double default 0 after balanceId;
-
