436c26abf53b6329dfcace317b86acffaea168f3
hiramvillarreal <hiramvillarreal@hotmail.com>
2016-07-07 09:18:39 -0600

    Show sale or logo graphics at totalgroup when not in used

--- iotpos-build/styles/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/earth/iotpos/styles
-# Build directory: /home/earth/iotpos/iotpos-build/styles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(simple)
-SUBDIRS(elegant)
